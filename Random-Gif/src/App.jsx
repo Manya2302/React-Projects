738e95abca20955da821c23d9a43a3e666779d93
Manya <parikhgaming@gmail.com>
2025-07-30 20:03:12 +0530

    Add GIF changes

---
+++ Random-Gif/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Heading from './components/Heading'
+import Random from './components/Random'
+import Tag from './components/Tag'
+
+const App = () => {
+  return (
+    <div className='w-full flex flex-col h-screen background relative items-center'>
+      <Heading />
+      <div className='flex flex-col w-full items-center mt-7 gap-y-10 '>
+        <Random />
+        <Tag />
+      </div>
+    </div>
+  )
+}
+
+export default App
